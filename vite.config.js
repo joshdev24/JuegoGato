047cf0a60721f04a04e13fdc147ecbf107d72f85
josh.dev <joshtowson16@gmail.com>
2025-07-28 05:01:08 -0300

    result

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'icon.png' , "fondo.jpg"],
-      manifest: {
-        name: 'Juego del Gato Rascado',
-        short_name: 'RascaGato',
-        description: 'Un juego divertido para rascar al gato',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'icon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
 });
