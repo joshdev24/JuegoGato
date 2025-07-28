ffd038dc3c1ccee8589ace533d149db6a966ae20
josh.dev <joshtowson16@gmail.com>
2025-07-28 03:44:01 -0300

    images y gifs a public

---
+++ src/Stars.jsx
@@ -0,0 +1,19 @@
+
+import { useEffect } from 'react';
+import confetti from 'canvas-confetti';
+
+export default function Stars() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      confetti({
+        particleCount: 5,
+        spread: 60,
+        origin: { x: Math.random(), y: Math.random() * 0.6 }
+      });
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return null; // No renderiza nada visible
+}
